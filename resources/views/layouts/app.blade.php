c9a907e5b4e94ed06b30c1bbdddc16827883d8ce
Ariel Calipay <83052833+arielcalipay20@users.noreply.github.com>
2026-01-14 13:15:33 +0800

    push

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
+    <link rel="icon" href="{{ asset('logo.ico') }}">
     <title>@yield('title', 'Calipay Portfolio')</title>
 
     <!-- Bootstrap CSS CDN -->
